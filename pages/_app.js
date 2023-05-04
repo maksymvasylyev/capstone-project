68a5a290f0d0c593eb72713ad655048a96ce71f4
Maksym Vasylyev <102429024+maksymvasylyev@users.noreply.github.com>
2023-05-04 11:45:31 +0200

    fetched data

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import GlobalStyle from "../styles";
+import { SWRConfig, useSWRConfig } from "swr";
+
+const fetcher = (URL) => fetch("data.json").then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig value={{ fetcher }}>
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }
