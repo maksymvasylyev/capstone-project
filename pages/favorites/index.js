81bff40442acb79962ad557bcb60acd03a52aa42
Maksym Vasylyev <102429024+maksymvasylyev@users.noreply.github.com>
2023-05-11 17:30:27 +0200

    add toggle function to favorite page

--- pages/favorites/index.js
+++ pages/favorites/index.js
@@ -1,6 +1,18 @@
 import Favorites from "@/components/Favorites/Favorites";
+import { useState } from "react";
 
-function FavoritePage({ cars, handleToggleFavorite }) {
+function FavoritePage({ cars, onToggleFavorite }) {
+  const [filteredCars, setFilteredCars] = useState(cars);
+
+  function handleToggleFavorite(id) {
+    const updatedFavCars = filteredCars.map((car) => {
+      if (car.id === id) {
+        return { ...car, isFavorite: !car.isFavorite };
+      } else return car;
+    });
+    setFilteredCars(updatedFavCars);
+    onToggleFavorite(id);
+  }
   return (
     <>
       <Favorites cars={cars} handleToggleFavorite={handleToggleFavorite} />
