ecab2dfbbca73416203d60d7896c3fcb9d684e51
Maksym Vasylyev <102429024+maksymvasylyev@users.noreply.github.com>
2023-05-19 16:47:35 +0200

    fix bug

--- components/AddCarForm/AddCarForm.js
+++ components/AddCarForm/AddCarForm.js
@@ -80,18 +80,13 @@ function AddCarForm({ cars, onAddCar, onDeleteCar, onToggleCompared }) {
   async function handleFileUpload(event) {
     event.preventDefault();
     setIsUploading(true);
-    // Create a new FormData object to store the image file and metadata
+
     const formData = new FormData();
-    // Append the image file to the FormData object with the key 'file'
+
     formData.append("file", image);
 
-    // Append the upload preset to the FormData object with the key 'upload_preset'
-    // The upload preset is a predefined set of options for uploading images to Cloudinary
-    // You can create one in the Cloudinary dashboard under Settings > Upload > Upload presets
     formData.append("upload_preset", process.env.NEXT_PUBLIC_UPLOAD_PRESET);
-    // Use the fetch API to send a POST request to the Cloudinary upload endpoint
-    // Replace <cloudname> with your own Cloudinary cloud name
-    // https://api.cloudinary.com/v1_1/<cloudname>/upload
+
     try {
       const response = await fetch(
         `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDNAME}/upload`,
