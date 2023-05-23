9d5736a4ce18dee84f438a72ce96a65ce2d6fd06
Maksym Vasylyev <102429024+maksymvasylyev@users.noreply.github.com>
2023-05-23 15:00:43 +0200

    add update form

--- components/AddCarForm/AddCarForm.js
+++ components/AddCarForm/AddCarForm.js
@@ -5,61 +5,13 @@ import AddCarList from "./AddCarList";
 import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
 import data from "../../data.json";
-
-const StyledHeader = styled.h2`
-  font-size: 1.7em;
-`;
-const StyledNewCarLabel = styled.label`
-  color: black;
-  margin: 0;
-`;
-
-const StyledNewCarSelect = styled.select`
-  margin: 0.5em;
-  padding: 0.5em;
-  border-radius: 5px;
-  border: none;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-`;
-
-const StyledNewCarInput = styled.input`
-  margin-bottom: 0.5em;
-  padding: 0.5em;
-  border-radius: 5px;
-  border: solid;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-`;
-
-const StyledAddCarButton = styled.button`
-  display: flex;
-  position: relative;
-  top: -10px;
-  right: -70px;
-  align-items: center;
-  font-family: inherit;
-  font-weight: 500;
-  font-size: 16px;
-  padding: 0.7em 1.4em 0.7em 1.1em;
-  color: white;
-  color: white;
-  background: black;
-  border: none;
-  box-shadow: 0 0.7em 1.5em -0.5em #14a73e98;
-  letter-spacing: 0.05em;
-  border-radius: 20em;
-  cursor: pointer;
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-
-  &:hover {
-    box-shadow: 0 0.5em 1.5em -0.5em red;
-  }
-
-  &:active {
-    box-shadow: 0 0.3em 1em -0.5em #14a73e98;
-  }
-`;
+import {
+  StyledAddCarButton,
+  StyledHeader,
+  StyledNewCarInput,
+  StyledNewCarLabel,
+  StyledNewCarSelect,
+} from "./StyledAddCarForm";
 
 function AddCarForm({ onDeleteCar, onToggleCompared }) {
   const [showForm, setShowForm] = useState(true);
--- components/UpdateCarForm/UpdateCarForm.js
+++ components/UpdateCarForm/UpdateCarForm.js
@@ -1,5 +1,147 @@
+import {
+  StyledAddCarButton,
+  StyledHeader,
+  StyledNewCarInput,
+  StyledNewCarLabel,
+  StyledNewCarSelect,
+} from "../AddCarForm/StyledAddCarForm";
+import StyledForm, { StyledSubmitButton } from "../FilterForm/StyledForm";
+
 function UpdateCarForm({ car }) {
-  return `${car.name}`;
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+  return (
+    <>
+      <StyledHeader>Edit Your Car</StyledHeader>
+
+      <StyledForm onSubmit={handleSubmit}>
+        {/* <StyledNewCarLabel htmlFor="avatar">
+          Please choose an image
+        </StyledNewCarLabel>
+        <StyledNewCarInput
+          type="file"
+          id="avatar"
+          onChange={handleFileChange}
+          value={imageValue}
+        /> */}
+
+        <StyledNewCarLabel htmlFor="name">Name:</StyledNewCarLabel>
+        <StyledNewCarInput name="name" id="name" defaultValue={car.name} />
+
+        <StyledNewCarLabel htmlFor="model">Model:</StyledNewCarLabel>
+        <StyledNewCarInput name="model" id="model" defaultValue={car.model} />
+
+        <StyledNewCarLabel htmlFor="bodyType">Body Type:</StyledNewCarLabel>
+        <StyledNewCarSelect
+          name="bodyType"
+          id="bodyType"
+          defaultValue={car.bodyType}
+        >
+          <option value="Sedan">Sedan</option>
+          <option value="SUV">SUV</option>
+          <option value="Coupe">Coupe</option>
+          <option value="VAN">VAN</option>
+          <option value="Cabriolet">Cabriolet</option>
+        </StyledNewCarSelect>
+
+        <StyledNewCarLabel htmlFor="wheelsDrive">
+          Wheels Drive:
+        </StyledNewCarLabel>
+        <StyledNewCarSelect
+          name="wheelsDrive"
+          id="wheelsDrive"
+          defaultValue={car.wheelsDrive}
+        >
+          <option value="awd">awd</option>
+          <option value="rwd">rwd</option>
+          <option value="fwd">fwd</option>
+        </StyledNewCarSelect>
+
+        <StyledNewCarLabel htmlFor="acceleration">
+          Acceleration (0-100km/h (s)):
+        </StyledNewCarLabel>
+        <StyledNewCarInput
+          type="number"
+          min={0}
+          name="acceleration"
+          id="acceleration"
+          defaultValue={car.acceleration}
+        />
+
+        <StyledNewCarLabel htmlFor="Fuel">Fuel:</StyledNewCarLabel>
+        <StyledNewCarSelect name="Fuel" defaultValue={car.Fuel} id="Fuel">
+          <option value="Benzin">Benzin</option>
+          <option value="Diesel">Diesel</option>
+          <option value="Hybrid">Hybrid</option>
+          <option value="Electro">Electro</option>
+        </StyledNewCarSelect>
+
+        <StyledNewCarLabel htmlFor="FuelEconomy">
+          Fuel Economy (l/100km):
+        </StyledNewCarLabel>
+        <StyledNewCarInput
+          type="number"
+          min={0}
+          name="FuelEconomy"
+          defaultValue={car.FuelEconomy}
+          id="FuelEconomy"
+        />
+
+        <StyledNewCarLabel htmlFor="TopSpeed">
+          Top Speed (km/h):
+        </StyledNewCarLabel>
+        <StyledNewCarInput
+          type="number"
+          min={0}
+          name="TopSpeed"
+          defaultValue={car.TopSpeed}
+          id="TopSpeed"
+        />
+
+        <StyledNewCarLabel htmlFor="Engine">Engine (L):</StyledNewCarLabel>
+        <StyledNewCarInput
+          type="number"
+          min={0}
+          name="Engine"
+          defaultValue={car.Engine}
+          id="Engine"
+        />
+
+        <StyledNewCarLabel htmlFor="Power">Power (HP):</StyledNewCarLabel>
+        <StyledNewCarInput
+          type="number"
+          min={0}
+          name="Power"
+          defaultValue={car.Power}
+          id="Power"
+        />
+
+        <StyledNewCarLabel htmlFor="CountryOfManufacture">
+          Country of Manufacture:
+        </StyledNewCarLabel>
+        <StyledNewCarInput
+          name="CountryOfManufacture"
+          defaultValue={car.CountryOfManufacture}
+          id="CountryOfManufacture"
+        />
+
+        <StyledNewCarLabel htmlFor="price">
+          Approx. Price (Euro):
+        </StyledNewCarLabel>
+        <StyledNewCarInput
+          type="number"
+          name="price"
+          defaultValue={car.price}
+          id="price"
+          min={1}
+          max={900000000}
+        />
+
+        <StyledSubmitButton type="Submit">Update</StyledSubmitButton>
+      </StyledForm>
+    </>
+  );
 }
 
 export default UpdateCarForm;
