0348970c5a2948506271aaa993dfc1fcaff3d500
Maksym Vasylyev <102429024+maksymvasylyev@users.noreply.github.com>
2023-05-25 15:38:01 +0200

    fix bugs

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,20 @@
 import useLocalStorageState from "use-local-storage-state";
 import GlobalStyle from "../styles";
+import data from "../data.json";
 import Layout from "@/components/Layout/Layout";
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { SWRConfig } from "swr";
 
-// const fetcher = (url) => fetch(url).then((response) => response.json());
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
-  const [showForm, setShowForm] = useState(true);
-  const router = useRouter();
-
+  const [isFormShown, setIsFormShown] = useState(true);
   const [cars, setCars] = useLocalStorageState("list", {
-    defaultValue: [],
+    defaultValue: data,
   });
 
-  async function fetchCars() {
-    const response = await fetch("/api/products");
-    const fetchedData = await response.json();
-    setCars(fetchedData);
-  }
-  useEffect(() => {
-    fetchCars();
-  }, []);
-
-  if (!cars) {
-    return "Loading...";
-  }
-
-  console.log(cars);
-
-  // const { data } = useSWR("/api/products");
-
-  // if (!data) {
-  //   return <h1>Loading...</h1>;
-  // }
-
+  const router = useRouter();
   function handleToggleFavorite(id) {
     const updatedCars = cars.map((car) => {
       if (car.id === id) {
@@ -44,8 +24,8 @@ export default function App({ Component, pageProps }) {
     setCars(updatedCars);
   }
 
-  function hideAddCarForm() {
-    setShowForm(!showForm);
+  function toggleVisibilityOfAddCarForm() {
+    setIsFormShown(!isFormShown);
   }
 
   function handleToggleCompared(id) {
@@ -73,7 +53,6 @@ export default function App({ Component, pageProps }) {
   }
 
   function handleEditCar(updatedCar, id) {
-    console.log(id);
     const updatedMyCars = cars.map((car) => {
       if (car.id === id) {
         return {
@@ -83,25 +62,24 @@ export default function App({ Component, pageProps }) {
       } else return car;
     });
     setCars(updatedMyCars);
-    console.log(cars);
   }
 
   return (
     <Layout>
       <GlobalStyle />
-      {/* <SWRConfig value={{ fetcher }}> */}
-      <Component
-        {...pageProps}
-        cars={cars}
-        onToggleFavorite={handleToggleFavorite}
-        onToggleCompared={handleToggleCompared}
-        clearComparedList={clearComparedList}
-        onDeleteCar={handleDeleteCar}
-        onEditCar={handleEditCar}
-        hideAddCarForm={hideAddCarForm}
-        showForm={showForm}
-      />
-      {/* </SWRConfig> */}
+      <SWRConfig value={{ fetcher }}>
+        <Component
+          {...pageProps}
+          cars={cars}
+          onToggleFavorite={handleToggleFavorite}
+          onToggleCompared={handleToggleCompared}
+          clearComparedList={clearComparedList}
+          onDeleteCar={handleDeleteCar}
+          onEditCar={handleEditCar}
+          toggleVisibilityOfAddCarForm={toggleVisibilityOfAddCarForm}
+          isFormShown={isFormShown}
+        />
+      </SWRConfig>
     </Layout>
   );
 }
