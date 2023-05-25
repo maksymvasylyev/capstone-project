fe529bbaa1ef2ed49ce3d6ac9ef3fdc0e180eb93
Maksym Vasylyev <102429024+maksymvasylyev@users.noreply.github.com>
2023-05-25 14:45:46 +0200

    clean code

--- pages/detailsCar/[id].js
+++ pages/detailsCar/[id].js
@@ -3,16 +3,6 @@ import data from "../../data.json";
 import CarDetails from "@/components/CarsDetails/CarDetails";
 
 function DetailPage() {
-  const router = useRouter();
-  const { id } = router.query;
-
-  const currentCar = data.find((car) => car.id === parseInt(id));
-
-  if (!currentCar) {
-    console.log("No data");
-    return <h1>{"No details are available"}</h1>;
-  }
-
   return <CarDetails car={currentCar} />;
 }
 
